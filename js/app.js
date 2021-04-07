c989a4b79a473350309ed97ca47b371b00b82983
Shivraj Hulbatte <shivrajhulbatte@gmail.com>
2021-04-08 01:33:35 +0530

    adding collapsable card for work and projects

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
-$(function(){
-    $(window).on("load resize",function(){
-        $(".fill-screen").css("height",window.innerHeight);
+$(function () {
+    $(window).on("load resize", function () {
+        $(".fill-screen").css("height", window.innerHeight);
     });
 
     $('body').scrollspy({
-        target:'.navbar',
-        offset:150
+        target: '.navbar',
+        offset: 150
     });
 
-    $('nav a').bind('click',function(){
-        $($(this).attr('href')).animatescroll({scrollspeed:2000,padding:50});
+    $('nav a').bind('click', function () {
+        $($(this).attr('href')).animatescroll({ scrollspeed: 2000, padding: 50 });
         event.preventDefault();
     });
 
-    $('.progress-bar').each(function() {
-        var bar_value = $(this).attr('aria-valuenow') + '%';                
+    $('.progress-bar').each(function () {
+        var bar_value = $(this).attr('aria-valuenow') + '%';
         $(this).animate({ width: bar_value }, { duration: 2000, easing: 'easeOutCirc' });
     });
 
@@ -22,3 +22,15 @@ $(function(){
     $('[data-toggle="github"]').tooltip();
     $('[data-toggle="hackerrank"]').tooltip();
 })
+// expand/collapse work experience card
+let cardExpanded = true;
+function expandCollapseCard(btn) {
+    console.log('>>btn', $(btn).next());
+    if(cardExpanded){
+        $(btn).children().css('transform', 'rotate(180deg)');
+    }else{
+        $(btn).children().css('transform', 'none');
+    }
+    cardExpanded = !cardExpanded;
+    $(btn).next().toggleClass("collapse-card");
+}
